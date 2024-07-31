cab6ec5a425ae9d7ea94d7dcdbf72f4a02c4e508
sebastian chkourko <chkse773@student.otago.ac.nz>
2024-07-31 13:32:09 +1200

    just added comments that need to be done

--- drum machine/drum machine/drum machine.cpp
+++ drum machine/drum machine/drum machine.cpp
@@ -113,8 +113,8 @@ int main() {
             int sound;
             std::cin >> sound;
             additionalSequence.push_back(sound);
-            std::cout << "Current Additional Sequence: ";
-            displaySequence(additionalSequence);
+            std::cout << "Current Additional Sequence: "; // need to make it so if we add a new sound it creates a new array which can 
+            displaySequence(additionalSequence);            // display it as an array.
             break;
         }
         case 3: {

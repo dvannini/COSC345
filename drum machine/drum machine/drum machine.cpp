a672d68e525aabb849fffcf220d11afa1e9dcdf3
sebastian chkourko <chkse773@student.otago.ac.nz>
2024-08-09 14:41:29 +1200

    added classes to avoid clutter

--- drum machine/drum machine/drum machine.cpp
+++ drum machine/drum machine/drum machine.cpp
@@ -16,19 +16,19 @@ void playSound(int sound) { // plays a sound according to the sequencer
     case 0:
         break;
     case 1:
-        PlaySound(L"../Assets/Kick 70s 1.wav", NULL, SND_FILENAME | SND_ASYNC);
+        PlaySound("../Assets/Kick 70s 1.wav", NULL, SND_FILENAME | SND_ASYNC);
         std::cout << "played kick." << std::endl;
         break;
     case 2:
-        PlaySound(L"../Assets/Snare 70s MPC 3.wav", NULL, SND_FILENAME | SND_ASYNC);
+        PlaySound("../Assets/Snare 70s MPC 3.wav", NULL, SND_FILENAME | SND_ASYNC);
         std::cout << "played snare." << std::endl;
         break;
     case 3:
-        PlaySound(L"../Assets/Hihat Closed 80s UK Disco Vinyl.wav", NULL, SND_FILENAME | SND_ASYNC);
+        PlaySound("../Assets/Hihat Closed 80s UK Disco Vinyl.wav", NULL, SND_FILENAME | SND_ASYNC);
         std::cout << "played HiHat." << std::endl;
         break;
     case 4:
-        PlaySound(L"../Assets/very-fat-808.wav", NULL, SND_FILENAME | SND_ASYNC);
+        PlaySound("../Assets/very-fat-808.wav", NULL, SND_FILENAME | SND_ASYNC);
         std::cout << "played 808." << std::endl;
         break;
     }

75573b8bf7a68da282bde965ebcb698c5f0908f8
Alex Haszard <aggis.haszard@gmail.com>
2024-08-21 20:40:43 -0700

    update tests

--- drum machine/UnitTesting/UnitTesting.cpp
+++ drum machine/UnitTesting/UnitTesting.cpp
@@ -2,6 +2,18 @@
 #include "CppUnitTest.h"
 #include "../drum machine/Sequence.cpp"
 #include "../drum machine/Audio_Engine.cpp"
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cstring>
+#include <assert.h>
+#include <vector>
+#include <chrono>
+#include <windows.h>
+#include <mmsystem.h>
+#include <string>
+#include <unordered_map>
+#pragma comment(lib, "winmm.lib")
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 

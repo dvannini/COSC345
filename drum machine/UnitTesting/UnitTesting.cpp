6f01fae1edac28c4c4ed17b05aca5df66d28a669
Alex Haszard <aggis.haszard@gmail.com>
2024-09-18 18:48:00 -0700

    Add clock tests, remove Keyboard class (unused)

--- drum machine/UnitTesting/UnitTesting.cpp
+++ drum machine/UnitTesting/UnitTesting.cpp
@@ -3,7 +3,6 @@
 #include "../drum machine/Sequence.cpp"
 #include "../drum machine/Audio_Engine.cpp"
 #include "../drum machine/Interface.cpp"
-#include "../drum machine/Keyboard.cpp"
 #include "../drum machine/Timeline.cpp"
 #include "../drum machine/Utils.cpp"
 #include "../drum machine/Clock.cpp"
@@ -14,7 +13,10 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 // Mock functions
 
 // Mock input for _getch
-int inputs[] = {0, 49, 97, 50, 49, 49, 13, 13, 72, 80, 13, 0, 0, 0, 0, 0, 0, 0, 0, 32};
+int inputs[] = {
+
+	0, 49, 97, 50, 49, 49, 13, 13, 72, 80, 13, 0, 0, 0, 0, 0, 0, 0, 0, 32 // Inputs for interface testing
+};
 int counter = -1;
 // Mock _getch() function
 int _getch() {
@@ -45,13 +47,17 @@ namespace UnitTesting
 			E.tick();
 			Assert::AreEqual(0, Audio_Engine::_test());
 		}
-		TEST_METHOD(TestKeyboard)
-		{
-			Assert::AreEqual(0, Keyboard::test_());
-		}
 		TEST_METHOD(TestClock)
 		{
-			Assert::AreEqual(0, Clock::test_());
+			int bpm = 120;
+			Clock c(bpm);
+			c.setBPM(115);
+			c.startClock();
+			// If running infinitely, this test is failing. Check the interval function.
+			bool isReached = false;
+			while (!isReached) {
+				isReached = c.interval();
+			}
 		}
 		TEST_METHOD(TestInterface)
 		{
